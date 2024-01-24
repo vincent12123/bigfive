4ff2a201070e6b7a614ce474f73d155d8b600b0a
Sukardi <35399885+vincent12123@users.noreply.github.com>
2024-01-24 18:43:34 +0700

    d

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, session
 from app import app, db, login_manager
 from app.models import  User, Question, Response, Result
 from flask_login import login_user, current_user, logout_user, login_required
@@ -21,10 +21,32 @@ def get_questions():
     questions = Question.query.all()
     return questions
 
-@app.route('/questionnaire')
-def questionnaire():
-    questions = get_questions()
-    random_questions = random.sample(questions, min(len(questions), 10))  # mengambil 10 pertanyaan secara acak, atau kurang jika tidak ada cukup pertanyaan
-    return render_template('questionnaire.html', questions=random_questions)
+@app.route('/questionnaire', defaults={'page': 1}, methods=['GET', 'POST'])
+@app.route('/questionnaire/<int:page>', methods=['GET', 'POST'])
+def questionnaire(page):
+    per_page = 10
+    total_questions = Question.query.count()
+    total_pages = total_questions // per_page + (1 if total_questions % per_page > 0 else 0)
 
+    if request.method == 'POST':
+        # Menyimpan jawaban dari halaman saat ini ke sesi
+        for key in request.form:
+            session[key] = request.form[key]
+        # Redirect ke halaman selanjutnya
+        next_page = page + 1
+        if next_page <= total_pages:
+            return redirect(url_for('questionnaire', page=next_page))
 
+    # Perbaikan di baris ini
+    questions = Question.query.order_by(Question.id).paginate(page=page, per_page=per_page, error_out=False).items
+
+    return render_template('questionnaire.html', questions=questions, page=page, total_pages=total_pages)
+
+@app.route('/submit-test', methods=['POST'])
+def submit_test():
+    all_responses = {key: value for key, value in session.items() if key.startswith('question_')}
+    # Proses jawaban untuk menyimpan ke database
+    # ...
+    # Kosongkan sesi setelah jawaban diproses
+    session.clear()
+    return redirect(url_for('result_page'))  # Asumsi ada route untuk menampilkan hasil
